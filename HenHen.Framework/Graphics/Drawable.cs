a4472767a0981769c4eb537a9d838a77300dd638
Hōtarō Oreki <TheZjarek@gmail.com>
2021-02-17 13:27:12 +0100

    HEN-15 - Make drawable abstract

--- HenHen.Framework/Graphics/Drawable.cs
+++ HenHen.Framework/Graphics/Drawable.cs
@@ -3,7 +3,7 @@ using System.Numerics;
 
 namespace HenHen.Framework.Graphics
 {
-    public class Drawable
+    public abstract class Drawable
     {
         public Vector2 Position { get; set; }
         public Axes RelativePositionAxes { get; set; }
