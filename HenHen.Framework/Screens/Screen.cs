1bbb3a00e975760e8a6366effa495cacb56499b3
Hōtarō Oreki <TheZjarek@gmail.com>
2021-02-17 15:14:07 +0100

    HEN-14 - More screen logic

--- HenHen.Framework/Screens/Screen.cs
+++ HenHen.Framework/Screens/Screen.cs
@@ -1,8 +1,15 @@
 ﻿using HenHen.Framework.Graphics;
+using System;
 
 namespace HenHen.Framework.Screens
 {
     public class Screen : Container, IContainer<Drawable>
     {
+        public event Action<Screen> ScreenPushed;
+        public event Action Exited;
+
+        public void Push(Screen nextScreen) => ScreenPushed?.Invoke(nextScreen);
+
+        public void Exit() => Exited?.Invoke();
     }
 }
--- HenHen.Framework/Screens/ScreenStack.cs
+++ HenHen.Framework/Screens/ScreenStack.cs
@@ -8,7 +8,7 @@ namespace HenHen.Framework.Screens
     {
         private readonly List<Screen> screens = new List<Screen>();
 
-        public Screen CurrentScreen => screens[^1];
+        public Screen CurrentScreen => screens.Count == 0 ? null : screens[^1];
 
         public IEnumerable<Screen> Children => screens;
 
@@ -18,13 +18,46 @@ namespace HenHen.Framework.Screens
         protected override void OnUpdate()
         {
             base.OnUpdate();
-            CurrentScreen.Update();
+            CurrentScreen?.Update();
         }
 
         protected override void OnRender()
         {
             base.OnRender();
-            CurrentScreen.Render();
+            CurrentScreen?.Render();
         }
+
+        public void Push(Screen screen)
+        {
+            var prev = CurrentScreen;
+            screen.Parent = this;
+            screens.Add(screen);
+            RewireEventObserving(prev);
+        }
+
+        public void Pop()
+        {
+            var prev = CurrentScreen;
+            CurrentScreen.Parent = null;
+            screens.Remove(CurrentScreen);
+            RewireEventObserving(prev);
+        }
+
+        private void RewireEventObserving(Screen previous)
+        {
+            if (previous is not null)
+            {
+                previous.Exited -= OnScreenExited;
+                previous.ScreenPushed -= OnScreenPushed;
+            }
+            if (CurrentScreen is not null)
+            {
+                CurrentScreen.Exited += OnScreenExited;
+                CurrentScreen.ScreenPushed += OnScreenPushed;
+            }
+        }
+
+        private void OnScreenPushed(Screen nextScreen) => Push(nextScreen);
+        private void OnScreenExited() => Pop();
     }
 }
