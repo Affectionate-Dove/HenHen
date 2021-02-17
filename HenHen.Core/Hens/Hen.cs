7a002bf4e3d8ba18b4425ec7b884e4aa4f50bcbe
Maciej Klimiuk <maciek120200@gmail.com>
2021-02-17 16:29:34 +0100

    HEN-12 Dodanie klasy Hen.

---
+++ HenHen.Core/Hens/Hen.cs
@@ -0,0 +1,14 @@
+﻿namespace HenHen.Core.Hens
+{
+    public class Hen
+    {
+        public int Health;
+        public HenStatistics HenStatistics;
+        public TypHena Type;
+
+    }
+    public enum TypHena
+    {
+        PROWADZACY, STUDENT, POMOCNIK
+    }
+}
---
+++ HenHen.Core/Hens/HenStatistics.cs
@@ -0,0 +1,9 @@
+﻿namespace HenHen.Core.Hens
+{
+    public class HenStatistics
+    {
+        public int Attack;
+        public int Defence;
+        public int Speed;
+    }
+}
