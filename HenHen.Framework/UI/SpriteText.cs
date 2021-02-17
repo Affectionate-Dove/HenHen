e71f06c28fa9e1771a330cf72576b3e8f711bd68
Hōtarō Oreki <TheZjarek@gmail.com>
2021-02-17 23:59:51 +0100

    HEN-17 - Add middle align option to SpriteText

--- HenHen.Framework/UI/SpriteText.cs
+++ HenHen.Framework/UI/SpriteText.cs
@@ -11,11 +11,26 @@ namespace HenHen.Framework.UI
 
         public ColorInfo Color { get; set; } = new ColorInfo(255, 255, 255);
 
+        public float FontSize { get; set; } = 16;
+
+        public float Spacing { get; set; } = 1;
+
+        public bool AlignMiddle { get; set; }
+
         protected override void OnRender()
         {
             base.OnRender();
             var r = GetRenderRect();
-            Raylib_cs.Raylib.DrawTextRec(Font, Text, new Raylib_cs.Rectangle(r.Left, r.Top, r.Width, r.Height), 12, 1, true, Color.ToRaylibColor());
+            var size = Raylib_cs.Raylib.MeasureTextEx(Font, Text, FontSize, Spacing);
+            var containingSize = GetRenderSize();
+
+            if (AlignMiddle && size.X <= containingSize.X)
+            {
+                var halfDiff = (containingSize - size) * 0.5f;
+                r.Location = new System.Drawing.PointF(r.Location.X + halfDiff.X, r.Location.Y + halfDiff.Y);
+            }
+
+            Raylib_cs.Raylib.DrawTextRec(Font, Text, new Raylib_cs.Rectangle(r.Left, r.Top, r.Width, r.Height), FontSize, Spacing, size.X > Size.X, Color.ToRaylibColor());
         }
     }
 }
