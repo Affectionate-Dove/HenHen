fe8b180f3c9f472c7ca1bb3806873f98a29a80a8
Hōtarō Oreki <TheZjarek@gmail.com>
2021-02-17 15:14:44 +0100

    HEN-14 - Add Window class and use it

---
+++ HenHen.Visual/Window.cs
@@ -0,0 +1,56 @@
+﻿using System.Numerics;
+
+namespace HenHen.Visual
+{
+    public class Window
+    {
+        public Window(Vector2 size, string title)
+        {
+            InitWindow(size, title);
+            TargetFPS = 60;
+        }
+
+        protected void InitWindow(Vector2 size, string title)
+        {
+            var x = (int)size.X;
+            var y = (int)size.Y;
+            this.size = new Vector2(x, y);
+            Raylib_cs.Raylib.InitWindow(x, y, title);
+        }
+
+        private Vector2 size;
+        public Vector2 Size 
+        { 
+            get => size;
+            set
+            {
+                var x = (int)value.X;
+                var y = (int)value.Y;
+                size = new Vector2(x, y);
+                Raylib_cs.Raylib.SetWindowSize(x, y);
+            }
+        }
+
+        private string title;
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+                Raylib_cs.Raylib.SetWindowTitle(title);
+            }
+        }
+
+        private int targetFPS;
+        public int TargetFPS
+        {
+            get => targetFPS;
+            set
+            {
+                targetFPS = value;
+                Raylib_cs.Raylib.SetTargetFPS(targetFPS);
+            }
+        }
+    }
+}
