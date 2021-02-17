4a4a22aa3bb706d0f8d4a7350cee5d57c440d0ab
Hōtarō Oreki <TheZjarek@gmail.com>
2021-02-17 23:12:49 +0100

    HEN-17 - Add SpriteText class

---
+++ HenHen.Framework/UI/SpriteText.cs
@@ -0,0 +1,21 @@
+﻿using HenHen.Framework.Extensions;
+using HenHen.Framework.Graphics;
+
+namespace HenHen.Framework.UI
+{
+    public class SpriteText : Drawable, IHasColor
+    {
+        public string Text { get; set; }
+
+        public Raylib_cs.Font Font { get; set; } = Raylib_cs.Raylib.GetFontDefault();
+
+        public ColorInfo Color { get; set; } = new ColorInfo(255, 255, 255);
+
+        protected override void OnRender()
+        {
+            base.OnRender();
+            var r = GetRenderRect();
+            Raylib_cs.Raylib.DrawTextRec(Font, Text, new Raylib_cs.Rectangle(r.Left, r.Top, r.Width, r.Height), 12, 1, true, Color.ToRaylibColor());
+        }
+    }
+}
