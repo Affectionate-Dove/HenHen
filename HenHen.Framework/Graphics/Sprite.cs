aca5ea53937f4180fd7821c835835fe56983c15b
Łukasz Krzysztoń <lukaszkrzyszton7@gmail.com>
2021-02-21 15:04:07 +0100

    HEN-26 - Create Sprite class

---
+++ HenHen.Framework/Graphics/Sprite.cs
@@ -0,0 +1,29 @@
+﻿using HenHen.Framework.Extensions;
+
+namespace HenHen.Framework.Graphics
+{
+    class Sprite : Drawable
+    {
+        public ColorInfo Color { get; set; } = new ColorInfo(255, 255, 255);
+        private Raylib_cs.Texture2D texture;
+
+        protected void LoadTexture(string path)
+        {
+            texture = Raylib_cs.Raylib.LoadTexture(path);
+        }
+        protected override void OnRender()
+        {
+            base.OnRender();
+            Raylib_cs.Raylib.DrawTexture(texture, (int)GetRenderPosition().X, (int)GetRenderPosition().Y, Color.ToRaylibColor());
+        }
+
+        protected void UnloadTexture(Raylib_cs.Texture2D texture)
+        {
+            Raylib_cs.Raylib.UnloadTexture(texture);
+        }
+        ~Sprite()
+        {
+            UnloadTexture(texture);
+        }
+    }
+}
