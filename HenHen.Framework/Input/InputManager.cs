54927ee3d5dbbf69f0d7950b4f22549ed0b1dac7
Hōtarō Oreki <TheZjarek@gmail.com>
2021-02-22 19:11:54 +0100

    HEN-19 - Make Input Manager not static and add MouseDelta

--- HenHen.Framework/Input/InputManager.cs
+++ HenHen.Framework/Input/InputManager.cs
@@ -2,13 +2,33 @@
 
 namespace HenHen.Framework.Input
 {
-    public static class InputManager
+    public class InputManager
     {
-        public static Vector2 MousePosition
+        private Vector2? mousePosition;
+
+        public Vector2? LastMousePosition { get; protected set; }
+
+        public Vector2 MousePosition
         {
-            get => Raylib_cs.Raylib.GetMousePosition(); 
-            set => Raylib_cs.Raylib.SetMousePosition((int)value.X, (int)value.Y);
+            get
+            {
+                if (mousePosition.HasValue)
+                    return mousePosition.Value;
+                return (mousePosition = Raylib_cs.Raylib.GetMousePosition()).Value;
+            }
+            set
+            {
+                mousePosition = value;
+                Raylib_cs.Raylib.SetMousePosition((int)value.X, (int)value.Y);
+            }
         }
+
+        /// <summary>
+        /// Returns Vector2.Zero if there is no delta.
+        /// </summary>
+        public Vector2 MousePositionDelta => (MousePosition - LastMousePosition).GetValueOrDefault();
+
+        public float TimeDelta { get; protected set; }
 
         public static float MouseWheelDelta => Raylib_cs.Raylib.GetMouseWheelMove();
 
@@ -21,5 +41,12 @@ namespace HenHen.Framework.Input
         public static bool IsMouseButtonUp(MouseButton button) => Raylib_cs.Raylib.IsMouseButtonUp(button.ToRaylibMouseButton());
         public static bool IsMouseButtonPressed(MouseButton button) => Raylib_cs.Raylib.IsMouseButtonPressed(button.ToRaylibMouseButton());
         public static bool IsMouseButtonReleased(MouseButton button) => Raylib_cs.Raylib.IsMouseButtonReleased(button.ToRaylibMouseButton());
+
+        public void Update(float timeDelta)
+        {
+            LastMousePosition = mousePosition;
+            MousePosition = Raylib_cs.Raylib.GetMousePosition();
+            TimeDelta = timeDelta;
+        }
     }
 }
