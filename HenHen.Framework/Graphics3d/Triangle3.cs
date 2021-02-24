65bb9784127c48b8324a1d4e1a68d94bab5837e1
Hōtarō Oreki <TheZjarek@gmail.com>
2021-02-25 00:39:31 +0100

    HEN-42 - Add Triangle3 struct

---
+++ HenHen.Framework/Graphics3d/Triangle3.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace HenHen.Framework.Graphics3d
+{
+    public struct Triangle3
+    {
+        public Vector3 A;
+        public Vector3 B;
+        public Vector3 C;
+
+        public Vector3 Centroid
+        {
+            get
+            {
+                var centerOfEdgeBC = (B + C) * 0.5f;
+                var median = centerOfEdgeBC - A;
+                return A + (median * 2 / 3f);
+            }
+        }
+
+        public Vector3 Normal => Vector3.Cross(A, B);
+    }
+}
