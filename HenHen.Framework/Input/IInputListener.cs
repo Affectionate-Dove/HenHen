39c4b56e9ff2741cac689e09c6a75a31ad0a7287
Maciej Klimiuk <maciek120200@gmail.com>
2021-02-24 17:54:33 +0100

    HEN-40 - Dodaj interfejs IInputListener

---
+++ HenHen.Framework/Input/IInputListener.cs
@@ -0,0 +1,12 @@
+﻿namespace HenHen.Framework.Input
+{
+    public interface IInputListener<TInputAction>
+    {
+        /// <summary>
+        /// If <see cref="TInputAction"/> was handled, <see cref="OnActionPressed"/>
+        /// should return <see cref="true"/>, otherwise <see cref="false"/>.
+        /// </summary>
+        public bool OnActionPressed(TInputAction action);
+        public void OnActionReleased(TInputAction action);
+    }
+}
