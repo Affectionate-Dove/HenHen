a63225678ce394a5e6d90ed808ec93fb87db0fde
Hōtarō Oreki <TheZjarek@gmail.com>
2021-02-24 11:29:18 +0100

    Expose TestInputManager and TestInputActionHandler for other future tests

--- HenHen.Framework.Tests/Input/InputActionHandlerTests.cs
+++ HenHen.Framework.Tests/Input/InputActionHandlerTests.cs
@@ -1,7 +1,5 @@
 ﻿using HenHen.Framework.Input;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
 
 namespace HenHen.Framework.Tests.Input
 {
@@ -61,46 +59,6 @@ namespace HenHen.Framework.Tests.Input
             Assert.IsFalse(actionHandler.AreActionKeysAllPressed(TestAction.Action1));
             Assert.IsFalse(actionHandler.AreActionKeysAllPressed(TestAction.Action2));
             inputManager.Update(0.02f);
-        }
-
-        private class TestInputActionHandler : InputActionHandler<TestAction>
-        {
-            public TestInputActionHandler(InputManager inputManager) : base(inputManager)
-            {
-            }
-
-            public override Dictionary<TestAction, List<KeyboardKey>> CreateDefaultKeybindings() => new Dictionary<TestAction, List<KeyboardKey>>
-            {
-                { TestAction.Action1, new List<KeyboardKey> { KeyboardKey.KEY_A } },
-                { TestAction.Action2, new List<KeyboardKey> { KeyboardKey.KEY_LEFT_CONTROL, KeyboardKey.KEY_S } },
-            };
-        }
-
-        private enum TestAction
-        {
-            Action1,
-            Action2
-        }
-
-        private class TestInputManager : InputManager
-        {
-            private readonly HashSet<KeyboardKey> justPressedKeys = new();
-            private readonly HashSet<KeyboardKey> pressedKeys = new();
-
-            public void SimulateKeyPress(KeyboardKey key)
-            {
-                justPressedKeys.Add(key);
-                pressedKeys.Add(key);
-            }
-
-            public void SimulateKeyRelease(KeyboardKey key) => pressedKeys.Remove(key);
-
-            public override bool IsKeyPressed(KeyboardKey key) => justPressedKeys.Contains(key);
-            public override bool IsKeyDown(KeyboardKey key) => pressedKeys.Contains(key);
-            public override bool IsKeyUp(KeyboardKey key) => !IsKeyDown(key);
-            public override bool IsKeyReleased(KeyboardKey key) => throw new NotImplementedException();
-
-            public override void Update(float timeDelta) => justPressedKeys.Clear();
         }
     }
 }
