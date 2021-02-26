d9bbb5e2c0962fe4b5fe5e4b0e92ed6f9c4964e7
Łukasz Krzysztoń <lukaszkrzyszton7@gmail.com>
2021-02-26 18:21:45 +0100

    HEN-39 - Create input propagator

---
+++ HenHen.Framework/Input/InputPropagator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace HenHen.Framework.Input
+{
+    public class InputPropagator<TInputAction>
+    {
+        private IInputListener<TInputAction> lastListener;
+
+        public List<IInputListener<TInputAction>> Listeners { get; } = new();
+
+        public void OnActionPressed(TInputAction action)
+        {
+            for (int i = Listeners.Count - 1; i >= 0; i--)
+            {
+                if (Listeners[i].OnActionPressed(action))
+                {
+                    lastListener = Listeners[i];
+                    break;
+                }
+            }
+        }
+
+        public void OnActionReleased(TInputAction action)
+        {
+            lastListener.OnActionReleased(action);
+        }
+    }
+}
