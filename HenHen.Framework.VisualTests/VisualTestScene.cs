90fa84edc5b3d3557e79a6b7a0dbabba0b5cc137
Hōtarō Oreki <TheZjarek@gmail.com>
2021-02-27 23:47:31 +0100

    Add VisualTester

---
+++ HenHen.Framework.VisualTests/VisualTestScene.cs
@@ -0,0 +1,11 @@
+﻿using HenHen.Framework.Screens;
+
+namespace HenHen.Framework.VisualTests
+{
+    public abstract class VisualTestScene : Screen
+    {
+        public bool IsSceneDone { get; protected set; }
+
+        public VisualTestScene() => RelativeSizeAxes = Graphics2d.Axes.Both;
+    }
+}
