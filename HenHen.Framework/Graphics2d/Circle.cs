c7d867fdd7a8bed582c5398814f993f61098035d
kubak <kubak@LAPTOP-0SJCO5LU>
2021-02-28 21:03:42 +0100

    HEN-50 - Add diameter, circumference, area properties.

--- HenHen.Framework/Graphics2d/Circle.cs
+++ HenHen.Framework/Graphics2d/Circle.cs
@@ -6,5 +6,18 @@ namespace HenHen.Framework.Graphics2d
     {
         public Vector2 CenterPosition;
         public float Radius;
+
+        public float Diameter 
+        {
+            get => 2 * Radius;
+            set => Radius = value;
+        }
+        public float Circumference
+        {
+            get => 2 * System.Math.PI * Radius;
+        }
+        public float Area
+        {
+            get => System.Math.PI * (Radius * Radius);
+        }
     }
-}
