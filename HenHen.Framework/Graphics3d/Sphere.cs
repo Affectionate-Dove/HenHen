b7a00174d1da193e40beaa65991d6a0aed671307
Maciej Klimiuk <maciek120200@gmail.com>
2021-03-01 13:56:03 +0100

    HEN-49 - Dodaj strukturę Sphere i testy

---
+++ HenHen.Framework/Graphics3d/Sphere.cs
@@ -0,0 +1,28 @@
+﻿using HenHen.Framework.Graphics2d;
+using System.Numerics;
+
+namespace HenHen.Framework.Graphics3d
+{
+    public struct Sphere
+    {
+        public Vector3 CenterPosition;
+        public float Radius;
+        public float SurfaceArea => 4 * System.MathF.PI * Radius * Radius;
+        public float Volume => 4 / 3f * System.MathF.PI * Radius * Radius * Radius;
+
+        public float Diameter
+        {
+            get => 2 * Radius;
+            set => Radius = value / 2;
+        }
+
+        public Circle ToTopDownCircle()
+        {
+            return new Circle
+            {
+                CenterPosition = new Vector2(CenterPosition.X, CenterPosition.Z),
+                Radius = Radius
+            };
+        }
+    }
+}
