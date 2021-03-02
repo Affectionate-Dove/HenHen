d72b5e6a3054bb46935b2679707851de4948380e
Łukasz Krzysztoń <lukaszkrzyszton7@gmail.com>
2021-03-02 15:40:34 +0100

    HEN-30 - Add Scene and Spatial

---
+++ HenHen.Framework/Graphics3d/Spatial.cs
@@ -0,0 +1,23 @@
+﻿using System.Numerics;
+
+namespace HenHen.Framework.Graphics3d
+{
+    public abstract class Spatial
+    {
+        public Vector3 Position { get; set; }
+        public Vector3 Rotation { get; set; }
+
+        public void Render()
+        {
+            OnRender();
+        }
+
+        protected virtual void OnRender()
+        {
+        }
+
+        protected virtual void OnUpdate()
+        {
+        }
+    }
+}
