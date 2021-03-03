9cafdce88bfd1227decbf639a9bf59ba74d18b12
Maciej Klimiuk <maciek120200@gmail.com>
2021-03-03 13:28:11 +0100

    Poprawki

--- HenHen.Framework/IO/Stores/ResourceStore.cs
+++ HenHen.Framework/IO/Stores/ResourceStore.cs
@@ -11,35 +11,21 @@ namespace HenHen.Framework.IO.Stores
         public void Unload(string assetName)
         {
             if (IsLoaded(assetName))
-            {
                 UnloadInternal();
-            }
             else
-            {
                 throw new System.Exception();
-            }
         }
+
+        public IEnumerable<string> LoadedAssetsNames() => loadedAssetsNames;
+
+        public bool IsLoaded(string assetName) => loadedAssetsNames.Contains(assetName);
 
         public T Get(string assetName)
         {
             if (IsLoaded(assetName))
-            {
                 return GetInternal();
-            }
             else
-            {
                 throw new System.Exception();
-            }
-        }
-
-        public IEnumerable<string> LoadedAssetsNames()
-        {
-            return loadedAssetsNames;
-        }
-
-        public bool IsLoaded(string assetName)
-        {
-            return loadedAssetsNames.Contains(assetName);
         }
 
         protected abstract void UnloadInternal();
