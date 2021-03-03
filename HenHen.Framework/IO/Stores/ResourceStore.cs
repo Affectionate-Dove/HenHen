26d70f23be2fedf7e3da064cd0cea81519df8db5
Maciej Klimiuk <maciek120200@gmail.com>
2021-03-03 13:13:41 +0100

    HEN-41 - Dodaj klasę ResourceStore

---
+++ HenHen.Framework/IO/Stores/ResourceStore.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace HenHen.Framework.IO.Stores
+{
+    public abstract class ResourceStore<T>
+    {
+        private readonly List<string> loadedAssetsNames = new();
+
+        public abstract void Load();
+
+        public void Unload(string assetName)
+        {
+            if (IsLoaded(assetName))
+            {
+                UnloadInternal();
+            }
+            else
+            {
+                throw new System.Exception();
+            }
+        }
+
+        public T Get(string assetName)
+        {
+            if (IsLoaded(assetName))
+            {
+                return GetInternal();
+            }
+            else
+            {
+                throw new System.Exception();
+            }
+        }
+
+        public IEnumerable<string> LoadedAssetsNames()
+        {
+            return loadedAssetsNames;
+        }
+
+        public bool IsLoaded(string assetName)
+        {
+            return loadedAssetsNames.Contains(assetName);
+        }
+
+        protected abstract void UnloadInternal();
+
+        protected abstract T GetInternal();
+    }
+}
