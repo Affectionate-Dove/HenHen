9350214194cbb8fd06f8b437fa858ccf8af0e34f
Łukasz Krzysztoń <lukaszkrzyszton7@gmail.com>
2021-03-04 19:35:21 +0100

    Test load nad unload functions

---
+++ HenHen.Framework.Tests/IO/Stores/ResourceStoreTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace HenHen.Framework.Tests.IO.Stores
+{
+    internal class ResourceStoreTests
+    {
+        private TestStore testStore = new();
+
+        [Test]
+        public void LoadTest()
+        {
+            string assetName = "some asset name";
+            testStore.Load(assetName);
+            Assert.IsTrue(testStore.IsLoaded(assetName));
+        }
+
+        [Test]
+        public void UnloadTest()
+        {
+            string assetName = "unloading test";
+            testStore.Load(assetName);
+            Assert.IsTrue(testStore.IsLoaded(assetName));
+            testStore.Unload(assetName);
+            Assert.IsFalse(testStore.IsLoaded(assetName));
+        }
+    }
+}
