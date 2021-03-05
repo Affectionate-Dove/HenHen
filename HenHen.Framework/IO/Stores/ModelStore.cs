f591c0adda1b090df52f26a1819be44591785174
kubak <kubak@LAPTOP-0SJCO5LU>
2021-03-05 14:58:19 +0100

    HEN - 59 - Add ModelStore

---
+++ HenHen.Framework/IO/Stores/ModelStore.cs
@@ -0,0 +1,16 @@
+﻿using Raylib_cs;
+using System.Collections.Generic;
+
+namespace HenHen.Framework.IO.Stores
+{
+    public class ModelStore : ResourceStore<Model>
+    {
+        private readonly Dictionary<string, Model> models = new();
+
+        protected override Model GetInternal(string assetName) => models[assetName];
+
+        protected override void LoadInternal(string assetName) => models.Add(assetName, Raylib.LoadModel(assetName));
+
+        protected override void UnloadInternal(string assetName) => Raylib.UnloadModel(GetInternal(assetName));
+    }
+}
