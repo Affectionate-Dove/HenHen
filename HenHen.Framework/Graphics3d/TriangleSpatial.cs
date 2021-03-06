83dfea5812ea3f6b283d4dadff2de4122159a093
Łukasz Krzysztoń <lukaszkrzyszton7@gmail.com>
2021-03-06 16:50:36 +0100

    HEN-43 - Add triangle spatial

---
+++ HenHen.Framework/Graphics3d/TriangleSpatial.cs
@@ -0,0 +1,15 @@
+﻿using HenHen.Framework.Extensions;
+using HenHen.Framework.Graphics2d;
+using HenHen.Framework.Numerics;
+
+namespace HenHen.Framework.Graphics3d
+{
+    public class TriangleSpatial : Spatial, IHasColor
+    {
+        public Triangle3 Triangle { get; set; }
+
+        public ColorInfo Color { get; set; } = new ColorInfo(255, 255, 255);
+
+        protected override void OnRender() => Raylib_cs.Raylib.DrawTriangle3D(Triangle.A, Triangle.B, Triangle.C, Color.ToRaylibColor());
+    }
+}
