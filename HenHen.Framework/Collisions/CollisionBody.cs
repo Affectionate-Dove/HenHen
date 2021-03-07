51d3726a30d14fbf96a62a60147aa64a6a818f53
Hōtarō Oreki <TheZjarek@gmail.com>
2021-03-07 11:37:24 +0100

    Override ToString in CollisionBody

--- HenHen.Framework/Collisions/CollisionBody.cs
+++ HenHen.Framework/Collisions/CollisionBody.cs
@@ -1,5 +1,5 @@
 ﻿// Copyright (c) Affectionate Dove <contact@affectionatedove.com>.
-// Licensed under the Affectionate Dove Limited Code Review License.
+// Licensed under the Affectionate Dove Limited Code Viewing License.
 // See the LICENSE file in the repository root for full license text.
 
 using HenHen.Framework.Numerics;
@@ -30,5 +30,7 @@ namespace HenHen.Framework.Collisions
                 maxDistance = Math.Max(maxDistance, sphere.CenterPosition.Length() + sphere.Radius);
             return maxDistance;
         }
+
+        public override string ToString() => $"{{CollisionBody: {Spheres}}}";
     }
 }
