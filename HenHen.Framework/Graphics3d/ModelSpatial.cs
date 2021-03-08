fcaf535727e58140acb56469e1cd12e01af80bf7
Maciej Klimiuk <maciek120200@gmail.com>
2021-03-08 15:31:34 +0100

    HEN-70 - Dodaj klasę ModelSpatial

---
+++ HenHen.Framework/Graphics3d/ModelSpatial.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Affectionate Dove <contact@affectionatedove.com>.
+// Licensed under the Affectionate Dove Limited Code Viewing License.
+// See the LICENSE file in the repository root for full license text.
+
+using HenHen.Framework.Extensions;
+using HenHen.Framework.Graphics2d;
+
+namespace HenHen.Framework.Graphics3d
+{
+    public class ModelSpatial : Spatial
+    {
+        private Raylib_cs.Model model;
+
+        public ColorInfo Color { get; set; } = new ColorInfo(255, 255, 255);
+
+        public void SetModel(string path) => model = Game.ModelStore.Get(path);
+
+        protected override void OnRender()
+        {
+            base.OnRender();
+            Raylib_cs.Raylib.DrawModel(model, Position, 1, Color.ToRaylibColor());
+        }
+    }
+}
