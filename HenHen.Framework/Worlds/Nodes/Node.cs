98241b3002fc6f2d58cd2dfce9e33ebe93590022
Maciej Klimiuk <maciek120200@gmail.com>
2021-03-08 15:03:41 +0100

    HEN-52 - Dodaj klasę Node

---
+++ HenHen.Framework/Worlds/Nodes/Node.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Affectionate Dove <contact@affectionatedove.com>.
+// Licensed under the Affectionate Dove Limited Code Viewing License.
+// See the LICENSE file in the repository root for full license text.
+
+using System;
+using System.Numerics;
+
+namespace HenHen.Framework.Worlds.Nodes
+{
+    public abstract class Node
+    {
+        public int Id { get; set; }
+        public Vector3 Position { get; set; }
+
+        public void Simulate(TimeSpan duration) => Simulation(duration);
+
+        protected virtual void Simulation(TimeSpan duration)
+        {
+        }
+    }
+}
