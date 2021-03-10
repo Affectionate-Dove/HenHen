7ed9066b1bd6d40523697de982c573b520530a98
Kubbak <kuba.kryk@gmail.com>
2021-03-10 17:56:42 +0100

    HEN-51 - Add Medium class

---
+++ HenHen.Framework/Worlds/Mediums/Medium.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Affectionate Dove <contact@affectionatedove.com>.
+// Licensed under the Affectionate Dove Limited Code Viewing License.
+// See the LICENSE file in the repository root for full license text.
+
+using HenHen.Framework.Numerics;
+
+namespace HenHen.Framework.Worlds.Mediums
+{
+    public class Medium
+    {
+        public MediumType Type { get; set; }
+        public Triangle3 Triangle { get; set; }
+    }
+
+    public enum MediumType
+    {
+        Ground,
+        Water,
+        Air
+    }
+}
