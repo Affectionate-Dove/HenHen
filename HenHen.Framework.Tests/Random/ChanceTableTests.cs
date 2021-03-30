aa54c6ce4239ca831e85864a5bca5ba7126b60af
Maciej Klimiuk <maciek120200@gmail.com>
2021-03-30 22:20:46 +0200

    HEN-84 - Dodaj rekord ChanceTable

---
+++ HenHen.Framework.Tests/Random/ChanceTableTests.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Affectionate Dove <contact@affectionatedove.com>.
+// Licensed under the Affectionate Dove Limited Code Viewing License.
+// See the LICENSE file in the repository root for full license text.
+
+using HenHen.Framework.Random;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HenHen.Framework.Tests.Random
+{
+    public class ChanceTableTests
+    {
+        [Test]
+        public void GetRandomTest()
+        {
+            var chanceTable = new ChanceTable<int>(new List<ChanceTableEntry<int>>
+            {
+                new ChanceTableEntry<int>(1,2),
+                new ChanceTableEntry<int>(2,4),
+                new ChanceTableEntry<int>(3,3)
+            });
+            var values = new List<int>();
+            for (var i = 0; i < 1000; i++)
+            {
+                values.Add(chanceTable.GetRandom());
+            }
+            Assert.IsTrue(values.Contains(1));
+            Assert.IsTrue(values.Contains(2));
+            Assert.IsTrue(values.Contains(3));
+            var amountOf1 = values.Count(value => value == 1);
+            var amountOf2 = values.Count(value => value == 2);
+            var amountOf3 = values.Count(value => value == 3);
+            Assert.IsTrue(amountOf1 < amountOf3);
+            Assert.IsTrue(amountOf3 < amountOf2);
+            Console.WriteLine("1: " + amountOf1);
+            Console.WriteLine("2: " + amountOf2);
+            Console.WriteLine("3: " + amountOf3);
+        }
+    }
+}
---
+++ HenHen.Framework/Random/ChanceTable.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Affectionate Dove <contact@affectionatedove.com>.
+// Licensed under the Affectionate Dove Limited Code Viewing License.
+// See the LICENSE file in the repository root for full license text.
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HenHen.Framework.Random
+{
+    public record ChanceTable<T>(IReadOnlyList<ChanceTableEntry<T>> Entries)
+    {
+        private static readonly System.Random random = new System.Random();
+        public T GetRandom()
+        {
+            var currentEndPoint = 0;
+            var sum = Entries.Sum(entry => entry.Chance);
+            var randomPoint = random.NextDouble() * sum;
+            for (var i = 0; i < Entries.Count; i++)
+            {
+                currentEndPoint += Entries[i].Chance;
+                if (randomPoint <= currentEndPoint)
+                {
+                    return Entries[i].Value;
+                }
+            }
+            throw new System.Exception("Couldn't pick random value.");
+        }
+    }
+}
