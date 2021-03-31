249ee9df62dddcab9cb5cc97d230094116c33713
Łukasz Krzysztoń <lukaszkrzyszton7@gmail.com>
2021-03-31 17:17:50 +0200

    HEN-63 - Implement Pathfinding Manager class

---
+++ HenHen.Framework/Worlds/PathFinding/PathfindingManager.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Affectionate Dove <contact@affectionatedove.com>.
+// Licensed under the Affectionate Dove Limited Code Viewing License.
+// See the LICENSE file in the repository root for full license text.
+
+using System.Collections.Generic;
+
+namespace HenHen.Framework.Worlds.PathFinding
+{
+    public class PathfindingManager
+    {
+        private int i = 0;
+        public List<Pathfinder> Pathfinders { get; } = new();
+
+        public void Update()
+        {
+            if (i == Pathfinders.Count) i = 0;
+            Pathfinders[i].Update();
+            i++;
+        }
+    }
+}
