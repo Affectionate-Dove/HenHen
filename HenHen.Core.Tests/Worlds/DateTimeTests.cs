521ffdcc52c342d0a16f30495990c42287f9d6cb
Kubbak <kuba.kryk@gmail.com>
2021-03-31 17:01:49 +0200

    Adjust functions

--- HenHen.Core.Tests/Worlds/DateTimeTests.cs
+++ HenHen.Core.Tests/Worlds/DateTimeTests.cs
@@ -11,13 +11,52 @@ namespace HenHen.Core.Tests.Worlds
     {
         [TestCase(6, 1)]
         [TestCase(19, 3)]
-        public void YearsTest(int years, int expected)
+        public void YearsTest(int months, int expected)
         {
-            var year = new DateTime
+            var dateTime = new DateTime
             {
-                Years = years
+                Months = months
             };
-            Assert.AreEqual(expected, year.Years);
+            Assert.AreEqual(expected, dateTime.Years);
+        }
+
+        [TestCase(7, 2, 2)]
+        [TestCase(16, 4, 4)]
+        [TestCase(1, 1, 1)]
+        public void MonthsTest(int weeks, int expectedMonths, int expectedMonth)
+        {
+            var dateTime = new DateTime
+            {
+                Weeks = weeks
+            };
+            Assert.AreEqual(expectedMonths, dateTime.Months);
+            Assert.AreEqual(expectedMonth, dateTime.Month);
+        }
+
+        [TestCase(3, 2, 2)]
+        [TestCase(7, 4, 4)]
+        [TestCase(18, 9, 1)]
+        public void SeasonsTest(int months, int expectedSeasons, int expectedSeason)
+        {
+            var dateTime = new DateTime
+            {
+                Months = months
+            };
+            Assert.AreEqual(expectedSeasons, dateTime.Seasons);
+            Assert.AreEqual(expectedSeason, dateTime.Season);
+        }
+
+        [TestCase(15, 3, 3)]
+        [TestCase(27, 5, 1)]
+        [TestCase(51, 8, 4)]
+        public void WeeksTest(int days, int expectedWeeks, int expectedWeek)
+        {
+            var dateTime = new DateTime
+            {
+                Days = days
+            };
+            Assert.AreEqual(expectedWeeks, dateTime.Weeks);
+            Assert.AreEqual(expectedWeek, dateTime.Week);
         }
     }
 }
--- HenHen.Core/Worlds/DateTime.cs
+++ HenHen.Core/Worlds/DateTime.cs
@@ -14,13 +14,31 @@ namespace HenHen.Core.Worlds
         private const int minutes_in_hour = 60;
         private const int seconds_in_minute = 60;
         private const int ticks_in_second = 60;
-        public int Years { get => Months / months_in_year; set => Months = value * months_in_year; }
-        public int Months { get => Weeks / weeks_in_month; set => Weeks = value * weeks_in_month; }
+
+        public int Years { get => Get(Months, months_in_year); set => Months = Set(value, months_in_year); }
+        public int Months { get => Get(Weeks, weeks_in_month); set => Weeks = Set(value, weeks_in_month); }
+        public int Month => Months % months_in_year;
         public int Seasons => (Months + 1) / 2;
-        public int Weeks { get => Days / 6; set => Days = value * days_in_week; }
-        public int Days { get => Hours / 24; set => Hours = value * hours_in_day; }
-        public int Hours { get => Minutes / 60; set => Minutes = value * minutes_in_hour; }
-        public int Minutes { get => Seconds / 60; set => Seconds = value * seconds_in_minute; }
-        public int Seconds { get => ticks / 60; set => ticks = value * ticks_in_second; }
+        public int Season => Seasons % 4;
+        public int Weeks { get => Get(Days, days_in_week); set => Days = Set(value, days_in_week); }
+        public int Week => Weeks % weeks_in_month;
+        public int Days { get => Get(Hours, hours_in_day); set => Hours = Set(value, hours_in_day); }
+
+        /// <summary>
+        /// Day in month.
+        /// </summary>
+        public int Day => Days % (days_in_week * weeks_in_month);
+
+        public int WeekDay => Days % days_in_week;
+        public int Hours { get => Get(Minutes, minutes_in_hour); set => Minutes = Set(value, minutes_in_hour); }
+        public int Hour => Hours % hours_in_day;
+        public int Minutes { get => Get(Seconds, seconds_in_minute); set => Seconds = Set(value, seconds_in_minute); }
+        public int Minute => Minutes % minutes_in_hour;
+        public int Seconds { get => Get(ticks, ticks_in_second); set => ticks = Set(value, ticks_in_second); }
+        public int Second => Seconds % seconds_in_minute;
+
+        private static int Get(int unit, int surroundingUnit) => (unit + (surroundingUnit - 1)) / surroundingUnit;
+
+        private static int Set(int unit, int surroundingUnit) => (unit * surroundingUnit) - (surroundingUnit - 1);
     }
 }
