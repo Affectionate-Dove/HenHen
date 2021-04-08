384b7875651400985e0479decaeef223767b99aa
Natalia Korzeniowska <korzenikn@gmail.com>
2021-04-08 14:00:59 +0200

    HEN-85 Add WorldViewer2d Class

---
+++ HenHen.Framework/Graphics2d/Worlds/WorldViewer2d.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Affectionate Dove <contact@affectionatedove.com>.
+// Licensed under the Affectionate Dove Limited Code Viewing License.
+// See the LICENSE file in the repository root for full license text.
+
+using HenHen.Framework.UI;
+
+namespace HenHen.Framework.Graphics2d.Worlds
+{
+    public class WorldViewer2d : Container
+    {
+        public WorldViewer2d()
+        {
+            AddChild(new Rectangle
+            {
+                Color = new ColorInfo(0, 60, 200),
+                RelativeSizeAxes = Axes.Both
+            });
+            AddChild(new SpriteText
+            {
+                Text = "Placeholder"
+            });
+        }
+    }
+}
