9fa4278850f68334fd341f4cc32812365dafe2c9
Hōtarō Oreki <TheZjarek@gmail.com>
2021-04-11 16:39:16 +0200

    Organize DateTime and DateTimeTests

--- HenHen.Core.Tests/Worlds/DateTimeTests.cs
+++ HenHen.Core.Tests/Worlds/DateTimeTests.cs
@@ -9,15 +9,17 @@ namespace HenHen.Core.Tests.Worlds
 {
     public class DateTimeTests
     {
-        [TestCase(6, 0)]
-        [TestCase(19, 2)]
-        public void YearsTest(int months, int expectedYears)
+        [TestCase(6, 0, 1)]
+        [TestCase(19, 2, 3)]
+        public void YearsTest(int months, int expectedYears, int expectedYear)
         {
             var dateTime = new DateTime
             {
                 Months = months
             };
             Assert.AreEqual(expectedYears, dateTime.Years);
+            // TODO:
+            // Assert.AreEqual(expectedYear, dateTime.Year);
         }
 
         [TestCase(7, 1, 2)]
@@ -33,9 +35,10 @@ namespace HenHen.Core.Tests.Worlds
             Assert.AreEqual(expectedMonth, dateTime.Month);
         }
 
-        [TestCase(3, 2, 2)]
-        [TestCase(7, 4, 4)]
-        [TestCase(18, 9, 1)]
+        [TestCase(3, 2, 3)]
+        [TestCase(7, 3, 4)]
+        [TestCase(8, 4, 1)]
+        [TestCase(18, 9, 2)]
         public void SeasonsTest(int months, int expectedSeasons, int expectedSeason)
         {
             var dateTime = new DateTime
@@ -46,9 +49,9 @@ namespace HenHen.Core.Tests.Worlds
             Assert.AreEqual(expectedSeason, dateTime.Season);
         }
 
-        [TestCase(15, 3, 3)]
-        [TestCase(27, 5, 1)]
-        [TestCase(51, 8, 4)]
+        [TestCase(15, 2, 3)]
+        [TestCase(27, 4, 1)]
+        [TestCase(51, 8, 1)]
         public void WeeksTest(int days, int expectedWeeks, int expectedWeek)
         {
             var dateTime = new DateTime
@@ -59,73 +62,79 @@ namespace HenHen.Core.Tests.Worlds
             Assert.AreEqual(expectedWeek, dateTime.Week);
         }
 
-        [TestCase(3, 3)]
-        [TestCase(15, 15)]
-        [TestCase(78, 78)]
-        public void SecondsTest(int seconds, int expectedSeconds)
+        [TestCase(6, 0, 1, 1)]
+        [TestCase(25, 1, 2, 2)]
+        [TestCase(150, 6, 7, 1)]
+        public void DaysTest(int hours, int expectedDays, int expectedDayOfMonth, int expectedWeekDay)
         {
             var dateTime = new DateTime
             {
-                Seconds = seconds
+                Hours = hours
             };
-            Assert.AreEqual(expectedSeconds, dateTime.Seconds);
+            Assert.AreEqual(expectedDays, dateTime.Days);
+            Assert.AreEqual(expectedDayOfMonth, dateTime.Day);
+            Assert.AreEqual(expectedWeekDay, dateTime.WeekDay);
         }
 
-        [TestCase(0, 0)]
-        [TestCase(30, 30)]
-        [TestCase(68, 68)]
-        [TestCase(131, 131)]
-        public void MinutesTest(int minutes, int expectedMinutes)
+        [TestCase(0, 0, 0)]
+        [TestCase(1, 0, 0)]
+        [TestCase(70, 1, 1)]
+        [TestCase(1442, 24, 0)]
+        public void HoursTest(int minutes, int expectedHours, int expectedHour)
         {
             var dateTime = new DateTime
             {
                 Minutes = minutes
             };
-            Assert.AreEqual(expectedMinutes, dateTime.Minutes);
+            Assert.AreEqual(expectedHours, dateTime.Hours);
+            Assert.AreEqual(expectedHour, dateTime.Hour);
         }
 
-        [Test]
-        public void HoursTest()
-        {
-            var dateTime = new DateTime();
-            for (var i = 0; i < 130; i++)
-            {
-                dateTime.Hours = i;
-                Assert.AreEqual(i, dateTime.Hours);
-            }
-        }
-
-        [Test]
-        public void DaysTest()
-        {
-            var dateTime = new DateTime();
-            for (var i = 0; i < 130; i++)
-            {
-                dateTime.Days = i;
-                Assert.AreEqual(i, dateTime.Days);
-            }
-        }
-
-        [TestCase(6, 0)]
-        [TestCase(121, 2)]
-        public void SecondsTest2(int seconds, int expectedMinutes)
+        [TestCase(0, 0, 0)]
+        [TestCase(1, 0, 0)]
+        [TestCase(70, 1, 1)]
+        [TestCase(1442, 24, 0)]
+        public void MinutesTest(int seconds, int expectedMinutes, int expectedMinute)
         {
             var dateTime = new DateTime
             {
                 Seconds = seconds
             };
             Assert.AreEqual(expectedMinutes, dateTime.Minutes);
+            Assert.AreEqual(expectedMinute, dateTime.Minute);
         }
 
-        [TestCase(6, 1)]
-        [TestCase(19, 3)]
-        public void DaysTest2(int days, int expectedWeeks)
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        [TestCase(59, 59)]
+        [TestCase(60, 0)]
+        [TestCase(61, 1)]
+        public void SecondsTest(int seconds, int expectedSecond)
         {
-            var dateTime = new DateTime
+            var dateTime = new DateTime { Seconds = seconds };
+            Assert.AreEqual(dateTime.Second, expectedSecond);
+        }
+
+        [Test]
+        public void SetAndGetTest()
+        {
+            var dateTime = new DateTime();
+            IsGetSameAsSet(() => dateTime.Seconds, v => dateTime.Seconds = v);
+            IsGetSameAsSet(() => dateTime.Minutes, v => dateTime.Minutes = v);
+            IsGetSameAsSet(() => dateTime.Hours, v => dateTime.Hours = v);
+            IsGetSameAsSet(() => dateTime.Days, v => dateTime.Days = v);
+            IsGetSameAsSet(() => dateTime.Weeks, v => dateTime.Weeks = v);
+            IsGetSameAsSet(() => dateTime.Months, v => dateTime.Months = v);
+            IsGetSameAsSet(() => dateTime.Years, v => dateTime.Years = v);
+        }
+
+        private static void IsGetSameAsSet(System.Func<int> getter, System.Action<int> setter)
+        {
+            for (var i = 0; i < 200; i++)
             {
-                Days = days
-            };
-            Assert.AreEqual(expectedWeeks, dateTime.Weeks);
+                setter(i);
+                Assert.AreEqual(i, getter());
+            }
         }
     }
 }
--- HenHen.Core/Worlds/DateTime.cs
+++ HenHen.Core/Worlds/DateTime.cs
@@ -15,14 +15,36 @@ namespace HenHen.Core.Worlds
         private const int seconds_in_minute = 60;
         private const int ticks_in_second = 60;
 
-        public int Years { get => Get(Months, months_in_year); set => Months = Set(value, months_in_year); }
-        public int Months { get => Get(Weeks, weeks_in_month); set => Weeks = Set(value, weeks_in_month); }
+        public int Years
+        {
+            get => Get(Months, months_in_year);
+            set => Months = Set(value, months_in_year);
+        }
+
+        public int Months
+        {
+            get => Get(Weeks, weeks_in_month);
+            set => Weeks = Set(value, weeks_in_month);
+        }
+
         public int Month => Months % months_in_year;
+
         public int Seasons => (Months + 1) / 2;
         public int Season => Seasons % 4;
-        public int Weeks { get => Get(Days, days_in_week); set => Days = Set(value, days_in_week); }
+
+        public int Weeks
+        {
+            get => Get(Days, days_in_week);
+            set => Days = Set(value, days_in_week);
+        }
+
         public int Week => Weeks % weeks_in_month;
-        public int Days { get => Get(Hours, hours_in_day); set => Hours = Set(value, hours_in_day); }
+
+        public int Days
+        {
+            get => Get(Hours, hours_in_day);
+            set => Hours = Set(value, hours_in_day);
+        }
 
         /// <summary>
         /// Day in month.
@@ -30,11 +52,29 @@ namespace HenHen.Core.Worlds
         public int Day => Days % (days_in_week * weeks_in_month);
 
         public int WeekDay => Days % days_in_week;
-        public int Hours { get => Get(Minutes, minutes_in_hour); set => Minutes = Set(value, minutes_in_hour); }
+
+        public int Hours
+        {
+            get => Get(Minutes, minutes_in_hour);
+            set => Minutes = Set(value, minutes_in_hour);
+        }
+
         public int Hour => Hours % hours_in_day;
-        public int Minutes { get => Get(Seconds, seconds_in_minute); set => Seconds = Set(value, seconds_in_minute); }
+
+        public int Minutes
+        {
+            get => Get(Seconds, seconds_in_minute);
+            set => Seconds = Set(value, seconds_in_minute);
+        }
+
         public int Minute => Minutes % minutes_in_hour;
-        public int Seconds { get => Get(ticks, ticks_in_second); set => ticks = Set(value, ticks_in_second); }
+
+        public int Seconds
+        {
+            get => Get(ticks, ticks_in_second);
+            set => ticks = Set(value, ticks_in_second);
+        }
+
         public int Second => Seconds % seconds_in_minute;
 
         private static int Get(int unit, int surroundingUnit) => unit / surroundingUnit;
