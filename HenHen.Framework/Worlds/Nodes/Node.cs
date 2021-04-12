378a38280f2d0c9863f232d83737ab441a0c9117
Hōtarō Oreki <TheZjarek@gmail.com>
2021-04-12 11:16:29 +0200

    Add SynchronizedTime to Node and add xmldocs

--- HenHen.Framework/Worlds/Nodes/Node.cs
+++ HenHen.Framework/Worlds/Nodes/Node.cs
@@ -8,19 +8,59 @@ using System.Numerics;
 
 namespace HenHen.Framework.Worlds.Nodes
 {
+    /// <summary>
+    ///     Represents anything in a <see cref="World"/>,
+    ///     from a simple information, to a physical object.
+    /// </summary>
     public abstract class Node
     {
+        /// <summary>
+        ///     Called when a new Node was created
+        ///     that should be placed into environment.
+        /// </summary>
         public event Action<Node> NodeEjected;
 
         public CollisionBody CollisionBody { get; set; }
         public int Id { get; set; }
         public Vector3 Position { get; set; }
 
-        public virtual void OnCollision(Node a)
+        /// <summary>
+        ///     The time at the beginning of
+        ///     the latest started simulation.
+        /// </summary>
+        public double SynchronizedTime { get; private set; }
+
+        public virtual void OnCollision(Node other)
         {
         }
 
-        public void Simulate(double duration) => Simulation(duration);
+        /// <summary>
+        ///     Updates <see cref="SynchronizedTime"/>
+        ///     and calls <see cref="Simulation(double)"/>
+        ///     if <paramref name="newTime"/> is greater than
+        ///     <see cref="SynchronizedTime"/>.
+        ///     If these are equal, does nothing.
+        /// </summary>
+        /// <param name="newTime">
+        ///     Has to be greater than or equal to
+        ///     <see cref="SynchronizedTime"/>.
+        /// </param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        ///     Thrown when <paramref name="newTime"/> is lesser than
+        ///     <see cref="SynchronizedTime"/>.
+        /// </exception>
+        public void Simulate(double newTime)
+        {
+            if (newTime < SynchronizedTime)
+                throw new ArgumentOutOfRangeException(nameof(newTime), $"Must be greater than or equal to {SynchronizedTime}");
+
+            if (newTime == SynchronizedTime)
+                return;
+
+            var duration = newTime - SynchronizedTime;
+            SynchronizedTime = newTime;
+            Simulation(duration);
+        }
 
         public override string ToString() => $"{GetType().Name}:{{" +
             $"{nameof(Id)}:{Id}," +
@@ -28,6 +68,12 @@ namespace HenHen.Framework.Worlds.Nodes
             $"{nameof(CollisionBody)}:{CollisionBody}" +
             $"}}";
 
+        /// <summary>
+        ///     Calls <see cref="NodeEjected"/>.
+        /// </summary>
+        /// <param name="node">
+        ///     The node that should be ejected.
+        /// </param>
         protected void EjectNode(Node node) => NodeEjected?.Invoke(node);
 
         protected virtual void Simulation(double duration)
