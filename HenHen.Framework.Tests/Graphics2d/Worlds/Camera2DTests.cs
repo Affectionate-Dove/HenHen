749aec51757c57455429cb0c99fd9d8d95908a03
Hōtarō Oreki <TheZjarek@gmail.com>
2021-05-09 16:00:47 +0200

    HEN-95 - Add Camera2D

---
+++ HenHen.Framework.Tests/Graphics2d/Worlds/Camera2DTests.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Affectionate Dove <contact@affectionatedove.com>.
+// Licensed under the Affectionate Dove Limited Code Viewing License.
+// See the LICENSE file in the repository root for full license text.
+
+using NUnit.Framework;
+
+namespace HenHen.Framework.Tests.Graphics2d.Worlds
+{
+    public class Camera2DTests
+    {
+        [Test]
+        public void GetVisibleArea() => throw new System.NotImplementedException();
+    }
+}
