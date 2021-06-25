b129591fdb22bc432d9a47d88eb5cf241914270c
Hōtarō Oreki <TheZjarek@gmail.com>
2021-06-25 22:53:01 +0200

    Add more tree testing

--- HenHen.Core.Tests/Worlds/Plants/TreeTests.cs
+++ HenHen.Core.Tests/Worlds/Plants/TreeTests.cs
@@ -5,12 +5,15 @@
 using HenHen.Core.Random;
 using HenHen.Core.Worlds;
 using HenHen.Core.Worlds.Plants;
+using HenHen.Framework.Worlds;
 using NUnit.Framework;
+using System.Linq;
 
 namespace HenHen.Core.Tests.Worlds.Plants
 {
     public class TreeTests
     {
+        private const int expected_drop_amount = 4;
         private TreeBreed treeBreed;
         private Tree tree;
 
@@ -21,9 +24,51 @@ namespace HenHen.Core.Tests.Worlds.Plants
             {
                 new(HenHenTime.FromSeconds(2), HenHenTime.FromSeconds(2)),
                 new(HenHenTime.FromSeconds(3), HenHenTime.FromSeconds(3)),
-            }, 4, new(HenHenTime.FromSeconds(3), HenHenTime.FromSeconds(3)), new[] { "Spring" });
+            }, expected_drop_amount, new(HenHenTime.FromSeconds(3), HenHenTime.FromSeconds(3)), new[] { "Spring" });
 
             tree = new Tree(treeBreed, new());
+        }
+
+        [Test]
+        public void CollectableTest()
+        {
+            var worldTime = 0.1;
+            tree.Simulate(worldTime);
+            Assert.AreEqual(0, tree.GrowthStage);
+            Assert.IsFalse(tree.Collectable);
+
+            worldTime += tree.GrowthStagesDuration[0].Seconds;
+            tree.Simulate(worldTime);
+            Assert.AreEqual(1, tree.GrowthStage);
+            Assert.IsFalse(tree.Collectable);
+
+            worldTime += tree.GrowthStagesDuration[1].Seconds;
+            tree.Simulate(worldTime);
+            Assert.AreEqual(2, tree.GrowthStage);
+            Assert.IsTrue(tree.Collectable);
+        }
+
+        [Test]
+        public void DropAmountTest() => Assert.AreEqual(expected_drop_amount, tree.DropAmount);
+
+        [Test]
+        public void InteractionTest()
+        {
+            var world = new World();
+            world.AddNode(tree);
+            world.Simulate(10);
+
+            Assert.IsTrue(tree.Collectable);
+            Assert.NotNull(tree.Interaction);
+            tree.Interaction();
+
+            Assert.IsFalse(tree.Collectable);
+            Assert.IsNull(tree.Interaction);
+
+            world.Simulate(10);
+            Assert.IsTrue(world.Nodes.Contains(tree));
+            Assert.NotZero(tree.DropAmount);
+            Assert.True(world.Nodes.Count(node => node is Fruit fruit && fruit.Name == tree.BreedName) == tree.DropAmount);
         }
     }
 }
