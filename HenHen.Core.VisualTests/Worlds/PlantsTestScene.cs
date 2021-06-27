2a532f4f579099a3afa172f664586b6957374581
Hōtarō Oreki <TheZjarek@gmail.com>
2021-06-27 19:32:22 +0200

    Finish plants test scene

--- HenHen.Core.VisualTests/Worlds/PlantsTestScene.cs
+++ HenHen.Core.VisualTests/Worlds/PlantsTestScene.cs
@@ -10,6 +10,10 @@ using HenHen.Framework.Graphics2d;
 using HenHen.Framework.Random;
 using HenHen.Framework.UI;
 using HenHen.Framework.VisualTests;
+using HenHen.Framework.Worlds.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 
 namespace HenHen.Core.VisualTests.Worlds
@@ -18,59 +22,66 @@ namespace HenHen.Core.VisualTests.Worlds
     {
         private readonly FlowerBreed flowerBreed;
         private readonly TreeBreed treeBreed;
-        private readonly Flower flower;
-        private readonly Tree tree;
+        private readonly Flower[] flowers;
+        private readonly Tree[] trees;
+        private readonly List<Node> nodes = new();
         private readonly SpriteText timeText;
 
         public PlantsTestScene()
         {
-            flowerBreed = new FlowerBreed(
-                Name: "Daisy",
-                GrowthStagesDurations: new RandomHenHenTimeRange[]
-                {
-                    new(HenHenTime.FromSeconds(2), HenHenTime.FromSeconds(6)),
-                    new(HenHenTime.FromSeconds(5), HenHenTime.FromSeconds(10))
-                },
-                DropAmount: new ChanceTable<int>(new ChanceTableEntry<int>[]
-                {
-                    new(2, 5),
-                    new(3, 2),
-                    new(4, 1)
-                }),
-                PossibleSeasons: new[] { "Spring" });
+            flowerBreed = CreateFlowerBreed();
+            treeBreed = CreateTreeBreed();
 
-            treeBreed = new TreeBreed(
-                Name: "Apple",
-                GrowthStagesDurations: new RandomHenHenTimeRange[]
-                {
-                    new(HenHenTime.FromSeconds(2), HenHenTime.FromSeconds(6)),
-                    new(HenHenTime.FromSeconds(5), HenHenTime.FromSeconds(10))
-                },
-                FruitsAmount: 3,
-                FruitsGrowthDuration: new RandomHenHenTimeRange(HenHenTime.FromSeconds(2), HenHenTime.FromSeconds(10)),
-                Seasons: new[] { "Spring" });
-
-            flower = new Flower(flowerBreed, HenHenTime.FromSeconds(0));
-            tree = new Tree(treeBreed, HenHenTime.FromSeconds(0));
-
-            AddChild(new FlowerDisplay(flower, flowerBreed)
+            var visualPlantsContainer = new Container
             {
-                Offset = new(-200, 200),
                 Anchor = new(0.5f),
-                Origin = new(0.5f, 1)
-            });
+                Origin = new(0.5f),
+                Size = new(600, 300)
+            };
+            AddChild(visualPlantsContainer);
 
-            var flowerInfo = "Flower growth stages durations:\n";
-            for (var i = 0; i < flower.GrowthStagesDuration.Count; i++)
-                flowerInfo += $"Stage {i + 1}: {flower.GrowthStagesDuration[i].Seconds:0}s\n";
-            AddChild(new SpriteText
+            const int trees_amount = 30;
+            trees = new Tree[trees_amount];
+            for (var i = 0; i < trees_amount; i++)
             {
-                Size = new(300, 100),
-                Offset = new(-200, 220),
-                Anchor = new(0.5f),
-                Origin = new(0.5f, 0),
-                Text = flowerInfo
-            });
+                trees[i] = new(treeBreed, HenHenTime.FromSeconds(0));
+                nodes.Add(trees[i]);
+                visualPlantsContainer.AddChild(new TreeDisplay(trees[i])
+                {
+                    Size = new(40, 120),
+                    Origin = new(0.5f, 1),
+                    RelativePositionAxes = Axes.Both,
+                    Offset = new((float)RNG.GetDouble(), (float)RNG.GetDouble())
+                });
+            }
+
+            const int flowers_amount = 70;
+            flowers = new Flower[flowers_amount];
+            for (var i = 0; i < flowers_amount; i++)
+            {
+                flowers[i] = new(flowerBreed, HenHenTime.FromSeconds(0));
+                nodes.Add(flowers[i]);
+                visualPlantsContainer.AddChild(new FlowerDisplay(flowers[i], flowerBreed)
+                {
+                    Size = new(20, 40),
+                    Origin = new(0.5f, 1),
+                    RelativePositionAxes = Axes.Both,
+                    Offset = new((float)RNG.GetDouble(), (float)RNG.GetDouble())
+                });
+            }
+            SortChildrenByDistance(visualPlantsContainer);
+
+            //var flowerInfo = "Flower growth stages durations:\n";
+            //for (var i = 0; i < flowers.GrowthStagesDuration.Count; i++)
+            //    flowerInfo += $"Stage {i + 1}: {flowers.GrowthStagesDuration[i].Seconds:0}s\n";
+            //AddChild(new SpriteText
+            //{
+            //    Size = new(300, 100),
+            //    Offset = new(-200, 220),
+            //    Anchor = new(0.5f),
+            //    Origin = new(0.5f, 0),
+            //    Text = flowerInfo
+            //});
 
             AddChild(timeText = new SpriteText
             {
@@ -84,15 +95,61 @@ namespace HenHen.Core.VisualTests.Worlds
 
         protected override void PreUpdate()
         {
-            flower.Simulate(flower.SynchronizedTime + 0.005f);
-            timeText.Text = flower.SynchronizedTime.ToString("0");
+            var newTime = nodes[0].SynchronizedTime + 0.005f;
+
+            foreach (var node in nodes)
+                node.Simulate(newTime);
+
+            timeText.Text = newTime.ToString("0");
             base.PreUpdate();
+
+            if (nodes.All(node => node.Interaction is not null))
+                InteractAll();
+        }
+
+        private static void SortChildrenByDistance(Container container) => container.Children.Sort((a, b) => Math.Sign(a.Offset.Y - b.Offset.Y));
+
+        private static FlowerBreed CreateFlowerBreed() => new
+        (
+            Name: "Daisy",
+            GrowthStagesDurations: new RandomHenHenTimeRange[]
+            {
+                new(HenHenTime.FromSeconds(2), HenHenTime.FromSeconds(6)),
+                new(HenHenTime.FromSeconds(5), HenHenTime.FromSeconds(10))
+            },
+            DropAmount: new ChanceTable<int>(new ChanceTableEntry<int>[]
+            {
+                new(2, 5),
+                new(3, 2),
+                new(4, 1)
+            }),
+            PossibleSeasons: new[] { "Spring" }
+        );
+
+        private static TreeBreed CreateTreeBreed() => new
+        (
+            Name: "Apple",
+            GrowthStagesDurations: new RandomHenHenTimeRange[]
+            {
+                new(HenHenTime.FromSeconds(2), HenHenTime.FromSeconds(6)),
+                new(HenHenTime.FromSeconds(5), HenHenTime.FromSeconds(10))
+            },
+            FruitsAmount: 3,
+            FruitsGrowthDuration: new RandomHenHenTimeRange(HenHenTime.FromSeconds(2), HenHenTime.FromSeconds(10)),
+            Seasons: new[] { "Spring" }
+        );
+
+        private void InteractAll()
+        {
+            foreach (var node in nodes)
+                node.Interaction();
         }
 
         private class FlowerDisplay : Container
         {
             private readonly Circle fruit;
             private readonly Container fruitContainer;
+            private readonly Rectangle stem;
 
             public Flower Flower { get; }
 
@@ -102,14 +159,17 @@ namespace HenHen.Core.VisualTests.Worlds
             {
                 Flower = flower;
                 Breed = breed;
-                AddChild(new Rectangle // stem
+                AddChild(stem = new Rectangle
                 {
-                    RelativeSizeAxes = Axes.Y,
-                    Size = new(20, 1),
-                    Color = new(0, 100, 0)
+                    RelativeSizeAxes = Axes.Both,
+                    Size = new(0.2f, 0),
+                    Color = new(0, 100, 0),
+                    Anchor = new(0.5f, 1),
+                    Origin = new(0.5f, 1)
                 });
                 AddChild(fruitContainer = new Container
                 {
+                    RelativeSizeAxes = Axes.Both,
                     Anchor = new(0.5f, 0),
                     Origin = new(0.5f),
                 });
@@ -127,7 +187,8 @@ namespace HenHen.Core.VisualTests.Worlds
                     var offset_vector = new Vector2(0, 0.5f).GetRotated(angle);
                     fruitContainer.AddChild(new Circle
                     {
-                        Size = new(38),
+                        RelativeSizeAxes = Axes.Both,
+                        Size = new(0.38f),
                         Origin = new(0.5f),
                         Anchor = new(0.5f),
                         Offset = offset_vector,
@@ -138,11 +199,105 @@ namespace HenHen.Core.VisualTests.Worlds
 
             protected override void PreUpdate()
             {
-                var height = 50 + (200 * Flower.GrowthStage / (float)Breed.GrowthStagesDurations.Count);
-                Size = new Vector2(20, height);
+                var stemHeight = 0.2f + (0.8f * Flower.GrowthStage / Breed.GrowthStagesDurations.Count);
+                stem.Size = new Vector2(0.2f, stemHeight);
                 fruit.Color = Flower.Collectable ? new(255, 180, 0) : new(0, 100, 0);
-                fruitContainer.Size = new(Flower.Collectable ? 100 : 0);
+                fruitContainer.Size = Flower.Collectable ? new(1, Size.X / Size.Y) : Vector2.Zero;
                 base.PreUpdate();
+            }
+        }
+
+        private class TreeDisplay : Container
+        {
+            private readonly Rectangle trunk;
+            private readonly Circle leaves;
+            private readonly Tree tree;
+            private readonly Container fruitsContainer;
+
+            public TreeDisplay(Tree tree)
+            {
+                this.tree = tree;
+                var brightness = RNG.GetDouble(0.5, 1);
+
+                AddChild(trunk = new Rectangle
+                {
+                    Color = new ColorInfo(70, 50, 20),
+                    Anchor = new(0.5f, 1),
+                    Origin = new(0.5f, 1),
+                    RelativeSizeAxes = Axes.Both
+                });
+                AddChild(leaves = new Circle
+                {
+                    Color = new ColorInfo(0, (byte)(180 * brightness), 0),
+                    Anchor = new(0.5f, 1),
+                    Origin = new(0.5f),
+                    RelativeSizeAxes = Axes.Both,
+                    RelativePositionAxes = Axes.Both
+                });
+                AddChild(fruitsContainer = new Container
+                {
+                    Anchor = new(0.5f, 1),
+                    Origin = new(0.5f),
+                    RelativeSizeAxes = Axes.Both,
+                    RelativePositionAxes = Axes.Both
+                });
+
+                var angle_difference = 360f / tree.DropAmount;
+                for (var i = 0; i < tree.DropAmount; i++)
+                {
+                    var angle = 42 + (angle_difference * i);
+                    var offset_vector = new Vector2(0, 0.3f).GetRotated(angle);
+                    fruitsContainer.AddChild(new Circle
+                    {
+                        Origin = new(0.5f),
+                        Anchor = new(0.5f),
+                        Offset = offset_vector,
+                        RelativePositionAxes = Axes.Both,
+                        RelativeSizeAxes = Axes.Both
+                    });
+                }
+            }
+
+            protected override void PreUpdate()
+            {
+                base.PreUpdate();
+                trunk.Size = CalculateTrunkSize();
+                var leavesWidth = CalculateLeavesRadius() * 2;
+                fruitsContainer.Size = leaves.Size = new(leavesWidth, leavesWidth * (Size.X / Size.Y));
+                fruitsContainer.Offset = leaves.Offset = new(0, -trunk.Size.Y);
+                var treeReady = tree.GrowthStage == tree.GrowthStagesDuration.Count;
+                var fruitsReady = tree.Collectable;
+                foreach (var fruit in fruitsContainer.Children.OfType<Circle>())
+                {
+                    fruit.Color = new((byte)(fruitsReady ? 255 : 50), (byte)(fruitsReady ? 0 : 60), 0, (byte)(treeReady ? 255 : 0));
+                    if (treeReady)
+                        fruit.Size = new(GetFruitSize());
+                }
+            }
+
+            private float GetFruitSize()
+            {
+                const float max_size = 0.1f;
+                var growthStart = tree.FruitsReadyDate - tree.FruitsGrowthDuration.Max;
+                var growingFor = HenHenTime.FromSeconds(tree.SynchronizedTime) - growthStart;
+                if (growingFor > tree.FruitsGrowthDuration.Max)
+                    return max_size;
+                return max_size * (float)(growingFor / tree.FruitsGrowthDuration.Max);
+            }
+
+            private Vector2 CalculateTrunkSize()
+            {
+                const float max_height = 1f;
+                const float min_height = 1 / 8f;
+                var height = min_height + (max_height * tree.GrowthStage / (tree.GrowthStagesDuration.Count + 1f));
+                var width = tree.GrowthStage == tree.GrowthStagesDuration.Count ? 0.3f : 0.2f;
+                return new(width, height);
+            }
+
+            private float CalculateLeavesRadius()
+            {
+                const float max_radius = 1;
+                return max_radius * tree.GrowthStage / tree.GrowthStagesDuration.Count;
             }
         }
     }
