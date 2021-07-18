ad00ef3d7157a0ece81ae04e21812ca756624fbb
Hōtarō Oreki <TheZjarek@gmail.com>
2021-07-18 16:03:49 +0200

    Add tests, documentation, Reversed() method to triangle structs

---
+++ HenHen.Framework.Tests/Numerics/Triangle3Tests.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Affectionate Dove <contact@affectionatedove.com>.
+// Licensed under the Affectionate Dove Limited Code Viewing License.
+// See the LICENSE file in the repository root for full license text.
+
+using HenHen.Framework.Numerics;
+using NUnit.Framework;
+using System.Numerics;
+
+namespace HenHen.Framework.Tests.Numerics
+{
+    internal class Triangle3Tests
+    {
+        private readonly Triangle3 triangle = new(new(0, 0, 4), new(0, 1.5f, 5), new(3, 0, 2));
+
+        [Test]
+        public void CentroidTest() => Assert.AreEqual(new Vector3(1, 0.5f, 3 + (2 / 3f)), triangle.Centroid);
+
+        [Test]
+        public void ReversedTest() => Assert.AreEqual(new Triangle3(new(3, 0, 2), new(0, 1.5f, 5), new(0, 0, 4)), triangle.Reversed());
+    }
+}
--- HenHen.Framework/Numerics/Triangle3.cs
+++ HenHen.Framework/Numerics/Triangle3.cs
@@ -6,12 +6,18 @@ using System.Numerics;
 
 namespace HenHen.Framework.Numerics
 {
+    /// <summary>
+    ///     Triangle in 3D space.
+    /// </summary>
     public readonly struct Triangle3
     {
         public Vector3 A { get; }
         public Vector3 B { get; }
         public Vector3 C { get; }
 
+        /// <summary>
+        ///     Mean position of all vertices.
+        /// </summary>
         public Vector3 Centroid
         {
             get
@@ -22,6 +28,10 @@ namespace HenHen.Framework.Numerics
             }
         }
 
+        /// <summary>
+        ///     The direction that the face of this
+        ///     <see cref="Triangle3"/> is pointing in.
+        /// </summary>
         public Vector3 Normal => Vector3.Cross(A, B);
 
         public Triangle3(Vector3 a, Vector3 b, Vector3 c)
@@ -40,6 +50,12 @@ namespace HenHen.Framework.Numerics
         public static Triangle3 operator /(Triangle3 triangle3, Vector3 v) => new(triangle3.A / v, triangle3.B / v, triangle3.C / v);
 
         public Triangle2 ToTopDownTriangle() => new(new Vector2(A.X, A.Z), new Vector2(B.X, B.Z), new Vector2(C.X, C.Z));
+
+        /// <summary>
+        ///     Creates a <see cref="Triangle3"/> with <see cref="C"/>
+        ///     and <see cref="A"/> vertices swapped.
+        /// </summary>
+        public Triangle3 Reversed() => new(C, B, A);
 
         public override string ToString() => $"{{{nameof(A)}={A},{nameof(B)}={B},{nameof(C)}={C}}}";
     }
