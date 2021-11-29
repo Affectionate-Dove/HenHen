66444ed007936e2b6149712325fb775fbb03fcec
Hōtarō Oreki <TheZjarek@gmail.com>
2021-11-29 20:58:54 +0100

    HEN-164 - Sprite.AutoFillModeProportions

--- HenHen.Framework/Graphics2d/Sprite.cs
+++ HenHen.Framework/Graphics2d/Sprite.cs
@@ -11,9 +11,42 @@ namespace HenHen.Framework.Graphics2d
     /// </summary>
     public class Sprite : Drawable
     {
-        public Raylib_cs.Texture2D Texture { get; set; }
+        private Raylib_cs.Texture2D texture;
+        private bool autoFillModeProportions;
+
+        public Raylib_cs.Texture2D Texture
+        {
+            get => texture;
+            set
+            {
+                texture = value;
+                if (AutoFillModeProportions)
+                    SetFillProportionsToTextureProportions();
+            }
+        }
 
         public ColorInfo Color { get; set; } = new ColorInfo(255, 255, 255);
+
+        /// <summary>
+        ///     Whether to automatically set
+        ///     <see cref="Drawable.FillModeProportions"/>
+        ///     to <see cref="Texture"/>'s proportions
+        /// </summary>
+        /// <remarks>
+        ///     If <see langword="true"/>, sets
+        ///     <see cref="Drawable.FillModeProportions"/> when
+        ///     this property or <see cref="Texture"/> changes.
+        /// </remarks>
+        public bool AutoFillModeProportions
+        {
+            get => autoFillModeProportions;
+            set
+            {
+                autoFillModeProportions = value;
+                if (autoFillModeProportions)
+                    SetFillProportionsToTextureProportions();
+            }
+        }
 
         protected override void OnRender()
         {
@@ -22,6 +55,16 @@ namespace HenHen.Framework.Graphics2d
             var sourceRec = new Raylib_cs.Rectangle(0, 0, Texture.width, Texture.height);
             var destRec = new Raylib_cs.Rectangle(rect.Left, rect.Top, rect.Width, rect.Height);
             Raylib_cs.Raylib.DrawTexturePro(Texture, sourceRec, destRec, Vector2.Zero, 0, Color);
+        }
+
+        /// <summary>
+        ///     Sets <see cref="Drawable.FillModeProportions"/>
+        ///     to proportions of <see cref="Texture"/>.
+        /// </summary>
+        private void SetFillProportionsToTextureProportions()
+        {
+            if (Texture.height is not 0)
+                FillModeProportions = Texture.width / (float)Texture.height;
         }
     }
 }
